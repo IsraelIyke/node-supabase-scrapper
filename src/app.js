699a1d61e4ba00c77a97da6d0c3a68d722482f42
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-22 17:43:15 +0100

    ok

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@ fetch("http://localhost:8000/results")
   .then((response) => {
     return response.json();
   })
-  .then((data) => console.log(data))
-  //   .then((data) => {
-  //     data.forEach((article) => {
-  //       const articleItem =
-  //         `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`;
-  //       feedDisplay.insertAdjacentHTML("beforeend", articleItem);
-  //     });
-  //   })
+  .then((data) => {
+    data.forEach((article) => {
+      const articleItem =
+        `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`;
+      feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+    });
+  })
   .catch((err) => console.log(err));
