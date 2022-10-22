31ec7e8be39b441d94dc50b1676d024e491ca9cb
IsraelIyke <nwangwuisrael@gmail.com>
2022-10-22 17:07:55 +0100

    ok

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-const feedDisplay = document.querySelector('#feed')
+const feedDisplay = document.querySelector("#feed");
 
-fetch('http://localhost:8000/results')
-    .then(response => {return response.json()})
-    .then(data => {
-        data.forEach(article => {
-            const articleItem = `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`
-            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
-        })
-    })
-    .catch(err => console.log(err))
-
-
+fetch("http://localhost:8000/results")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  //   .then((data) => {
+  //     data.forEach((article) => {
+  //       const articleItem =
+  //         `<div><h3>` + article.title + `</h3><p>` + article.url + `</p></div>`;
+  //       feedDisplay.insertAdjacentHTML("beforeend", articleItem);
+  //     });
+  //   })
+  .catch((err) => console.log(err));
